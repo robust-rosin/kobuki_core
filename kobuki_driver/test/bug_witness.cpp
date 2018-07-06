1e23d5ea1d8944819165d43ba13689eedf5285b1
git-afsantos <contact.andre.santos@gmail.com>
2018-07-06 22:41:27 +0100

    removed unnecessary dependency on ros

--- kobuki_driver/test/bug_witness.cpp
+++ kobuki_driver/test/bug_witness.cpp
@@ -1,5 +1,4 @@
 #include "kobuki_driver/kobuki.hpp"
-#include <ros/ros.h>
 #include <gtest/gtest.h>
 
 // Declare a test
@@ -12,7 +11,5 @@ TEST(TestSuite, testCase)
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "tester");
-  ros::NodeHandle nh;
   return RUN_ALL_TESTS();
 }
